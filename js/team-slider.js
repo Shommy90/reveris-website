77c262566235d9d1fa0993b8a77b079ce80c0efc
Milovan <marjanovic90@gmail.com>
2020-12-10 18:18:47 +0100

    add the rest of the team with avatar images, refactor js code

---
+++ js/team-slider.js
@@ -0,0 +1,31 @@
+function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
+
+  console.log('Width:  ' +  getWidth())
+
+  function slides() {
+    var slides = 3
+    if (getWidth() > 768) {
+      slides = 3
+    } if (getWidth() < 769) {
+      slides = 2
+    } if (getWidth() < 500) {
+      slides = 1
+    }
+
+    return slides
+  }
+
+  $('.autoplay').slick({
+    slidesToShow: slides(),
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+  });
