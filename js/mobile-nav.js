245a814e1ff684847514c4395a73b7628f34d479
Milovan <marjanovic90@gmail.com>
2020-12-30 11:21:22 +0100

    add new Entertainment images, add mobile btn animation

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -2,4 +2,7 @@ $('#toggle').click(function() {
     $('ul.mobile-nav').slideToggle(200, function() {
         // Animation complete.
     });
+
+    // Animate burger btn
+    $(this).toggleClass("open");
 });
